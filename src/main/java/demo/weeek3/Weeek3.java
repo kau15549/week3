5fbd5c990c4a6865ea04c094e55ba554542fd8a4
kau15549 <harpreetsidhu1205@gmail.com>
2024-01-26 12:52:33 -0500

    inital commit

---
+++ src/main/java/demo/weeek3/Weeek3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package demo.weeek3;
+
+/**
+ *
+ * @author User
+ */
+public class Weeek3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
