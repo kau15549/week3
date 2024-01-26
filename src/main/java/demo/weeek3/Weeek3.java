91c600f5d3598f23c6ec5306930b4bf1b793d722
kau15549 <harpreetsidhu1205@gmail.com>
2024-01-26 13:05:40 -0500

    inital commit

--- src/main/java/demo/weeek3/Weeek3.java
+++ src/main/java/demo/weeek3/Weeek3.java
@@ -11,6 +11,8 @@ package demo.weeek3;
 public class Weeek3 {
 
     public static void main(String[] args) {
+             System.out.println("Hello harpreet!");
         System.out.println("Hello World!");
+        //this is in my branch
     }
 }
